6226988e857cc56e22fb2134d26430eb8622d729
Omar Adel <omaradel@Omars-MacBook-Pro.local>
2026-01-27 22:40:29 +0200

    install prettier & create static app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
+
 function App() {
+  const step = 1;
 
   return (
-    <>
-      <h1>Hello Omar!</h1>
-    </>
-  )
+    <div className="steps">
+      <div className="numbers">
+        {messages.map((_message: string, index: number) => (
+          <div key={index} className={`${step >= index + 1 ? 'active' : ''}`}>
+            {index + 1}
+          </div>
+        ))}
+      </div>
+
+      <div className="message">
+        Step {step}: {messages[step - 1]}
+      </div>
+
+      <div className="buttons">
+        <button type="button">Previous</button>
+        <button type="button">Next</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
